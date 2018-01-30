1fd3378619227c073185ff9773086e4a9a670ad0
Davide Cascone <d.cascone@greenvulcano.com>
2018-01-30 18:31:15 +0100

    initial commit

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform
+} from 'react-native';
+import {
+  StackNavigator,
+} from 'react-navigation';
+
+import SearchPage from './SearchPage';
+import SearchResults from './SearchResults';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+
+const App = StackNavigator({
+  Home: { screen: SearchPage },
+  Results: { screen: SearchResults }
+});
+export default App;
